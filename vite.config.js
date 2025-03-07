1a0683fa37f74d19179da735854a2b513639c3c0
belaid.d <belaid.d@ambs-agency.com>
2025-03-07 11:46:35 +0100

    added the submneus links section to the header

--- vite.config.js
+++ vite.config.js
@@ -3,29 +3,21 @@ import tailwindcss from '@tailwindcss/vite';
 import { createHtmlPlugin } from 'vite-plugin-html';
 
 export default defineConfig({
-  plugins: [
-    tailwindcss({
-      config: {
-        content: ['./index.html', './homepage/index.html', './src/**/*.{js,jsx,ts,tsx}'],
-        theme: {
-          extend: {},
-        },
-        plugins: [],
-      },
-    }),
-    createHtmlPlugin({
-      pages: [
-        {
-          entry: '/src/main.js', // Entry point for the main app
-          filename: 'index.html', // Main HTML file
-          template: 'index.html', // Template for the main HTML file
-        },
-        {
-          entry: '/src/main.js', // Entry point for the homepage (optional)
-          filename: 'homepage/index.html', // Path to the homepage HTML file
-          template: 'homepage/index.html', // Template for the homepage HTML file
-        },
-      ],
-    }),
-  ],
+	plugins: [
+		tailwindcss(),
+		createHtmlPlugin({
+			pages: [
+				{
+					entry: '/src/main.js',
+					filename: 'index.html',
+					template: 'index.html',
+				},
+				{
+					entry: '/src/main.js',
+					filename: 'homepage/index.html',
+					template: 'homepage/index.html',
+				},
+			],
+		}),
+	],
 });
